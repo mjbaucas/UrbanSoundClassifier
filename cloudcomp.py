c427229b023a9ba09798810ed1ae88307fdc39b1
MBaucas <mjbaucas@gmail.com>
2019-06-11 17:11:25 -0400

    added data processing code

--- cloudcomp.py
+++ cloudcomp.py
@@ -7,10 +7,9 @@ if __name__ == "__main__":
 	
 	time_file = open("cloudtime.txt", "a+")
 	
-	elapse = time.time()
 	for i in range(0, 20):
-		elapse = time.time() - elapse
-		time_file.write("Start {}: {}\n".format(i, elapse))
+		lapse = time.time()
+		time_file.write("Start {}: {}\n".format(i, lapse))
 		os.system("python3 ../SoundRecorder/soundrec.py {}".format(file_name))
 		
 		sound_file = open(file_name, "rb")
@@ -18,13 +17,13 @@ if __name__ == "__main__":
 		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		client.connect(("10.16.10.240", 32500))
 		
-		elapse = time.time() - elapse
-		time_file.write("Middle {}: {}\n".format(i, elapse))
+		lapse = time.time()
+		time_file.write("Middle {}: {}\n".format(i, lapse))
 		while sound_data:
 			sent = client.send(sound_data)
 			if not sent:
 				break
 			sound_data = sound_data[sent:]
 			print(sound_data)			
-		elapse = time.time() - elapse
-		time_file.write("End {}: {}\n".format(i, elapse))
+		lapse = time.time()
+		time_file.write("End {}: {}\n".format(i, lapse))
