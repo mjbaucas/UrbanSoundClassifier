b09706ac561deea808601aeea94da165e9b6b347
mjbaucas <mjbaucas@gmail.com>
2019-06-18 17:21:05 -0400

    Redid recording experiment again

--- recordertests.py
+++ recordertests.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 	
 	time_file = open("recordtime.txt", "a+")
 	
-	for i in range(0, 20):
+	for i in range(0, 30):
 		lapse = time.time()
 		time_file.write("Start {}: {}\n".format(i, lapse))
 		os.system("python3 ../SoundRecorder/soundrec.py {}".format(file_name))
