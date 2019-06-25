f9dee3621ce164bbb84a518294ec5dfa5c830c18
mjbaucas <mjbaucas@gmail.com>
2019-06-25 18:15:16 -0400

    Endclient for simulation

---
+++ endclient.py
@@ -0,0 +1,36 @@
+import os
+import time
+import socket
+
+if __name__ == "__main__":	
+	exit_client = False
+	while exit_client == False:	
+		file_name = "testsound.wav"
+
+		sound_file = open(file_name, "rb")
+		sound_data = sound_file.read()
+		
+		connected = False
+		while connected == False:
+			try:
+				print("Connecting to server")
+				client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				client.connect(("10.11.148.56", 32500))
+				connected = True
+			except Exception as e:
+				connected = False
+				# Do nothing
+		
+		while sound_data:
+			data = sound_data[:2048]
+			if not data:
+				break
+			try:
+				sent = client.send(data)
+				sound_data = sound_data[sent:]
+			except Exception as e:
+				break
+			
+		from_server = client.recv(1024)
+
+	
